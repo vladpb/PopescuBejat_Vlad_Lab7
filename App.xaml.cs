e2e386da5efbf071f2913ddfc9e65e135d21a493
vladpb <vlad.bejat@gmail.com>
2023-11-27 22:31:19 +0200

    -Laborator8

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,7 +1,25 @@
-﻿namespace PopescuBejat_Vlad_Lab7;
+﻿using System;
+using PopescuBejat_Vlad_Lab7.Data;
+using System.IO;
+
+
+namespace PopescuBejat_Vlad_Lab7;
 
 public partial class App : Application
-{
+{ 
+    static ShoppingListDatabase database;
+    public static ShoppingListDatabase Database
+    {
+        get
+        {
+            if (database == null)
+            {
+                database = new ShoppingListDatabase(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "ShoppingList.db3"));
+            }
+            return database;
+        }
+    }
+
 	public App()
 	{
 		InitializeComponent();
---
+++ Models/ShopList.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SQLite;
+
+
+namespace PopescuBejat_Vlad_Lab7.Models
+{   public class ShopList
+    {
+        [PrimaryKey, AutoIncrement]
+        public int ID { get; set; }
+        [MaxLength(250), Unique]
+        public string Description { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
