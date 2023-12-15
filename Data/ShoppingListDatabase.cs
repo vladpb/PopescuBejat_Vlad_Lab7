ffd2e6efde76e7f3513e3b40f3a5101ae1d48e4f
vladpb <vlad.bejat@gmail.com>
2023-12-15 11:49:24 +0200

    -lab10

--- Data/ShoppingListDatabase.cs
+++ Data/ShoppingListDatabase.cs
@@ -32,6 +32,7 @@ namespace PopescuBejat_Vlad_Lab7.Data
             _database.CreateTableAsync<ShopList>().Wait();
             _database.CreateTableAsync<Product>().Wait();
             _database.CreateTableAsync<ListProduct>().Wait();
+            _database.CreateTableAsync<Shop>().Wait();
         }
         public Task<List<ShopList>> GetShopListsAsync()
         {
@@ -77,5 +78,22 @@ namespace PopescuBejat_Vlad_Lab7.Data
         {
             return _database.Table<Product>().ToListAsync();
         }
+
+        public Task<List<Shop>> GetShopsAsync()
+        {
+            return _database.Table<Shop>().ToListAsync();
+        }
+        public Task<int> SaveShopAsync(Shop shop)
+        {
+            if (shop.ID != 0)
+            {
+                return _database.UpdateAsync(shop);
+            }
+            else
+            {
+                return _database.InsertAsync(shop);
+            }
+        }
     }
+
 }
--- Models/ShopList.cs
+++ Models/ShopList.cs
@@ -4,15 +4,16 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using SQLite;
+using SQLiteNetExtensions.Attributes;
 
-
-namespace PopescuBejat_Vlad_Lab7.Models
-{   public class ShopList
-    {
-        [PrimaryKey, AutoIncrement]
-        public int ID { get; set; }
-        [MaxLength(250), Unique]
-        public string Description { get; set; }
-        public DateTime Date { get; set; }
-    }
+namespace PopescuBejat_Vlad_Lab7.Models;
+public class ShopList
+{
+    [PrimaryKey, AutoIncrement]
+    public int ID { get; set; }
+    [MaxLength(250), Unique]
+    public string Description { get; set; }
+    public DateTime Date { get; set; }
+    [ForeignKey(typeof(Shop))]
+    public int ShopID { get; set; }
 }
